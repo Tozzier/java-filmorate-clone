6042608e2125feb6de003d6d5624799969027add
Tozier <java_Yap@icloud.com>
2024-10-07 16:43:33 +0400

    Ready

---
+++ src/main/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+@AllArgsConstructor
+@Data
+public class Film {
+    private int id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private long duration;
+}
+
+
